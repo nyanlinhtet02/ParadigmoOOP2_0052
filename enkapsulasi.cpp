c896bacbddfc902d825a49dbf1231d4cfd4db9fc
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-06-02 04:31:56 -0700

    Set lamp names at specific indices

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -21,10 +21,10 @@ int main ()
 { 
     remoteLampu lampuRumah; 
  
-    lampuRumah.setSaklarNo(0, "lampu teras rumah"); 
-    lampuRumah.setSaklarNo(1, "lampu ruang tamu"); 
-    lampuRumah.setSaklarNo(2, "lampu kamar tidur"); 
-    lampuRumah.setSaklarNo(3, "lampu dapur"); 
+    lampuRumah.setSaklarNo(0, "lampu teras rumah" ); 
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu" ); 
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur" ); 
+    lampuRumah.setSaklarNo(3, "lampu dapur" ); 
  
     cout << lampuRumah.getSaklarNo(0) << endl; 
     cout << lampuRumah.getSaklarNo(1) << endl; 
